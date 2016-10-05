fed5cb0fd1542f5f0dcac5f686d06251b7b7cb33
aayushi jain <aayushi@urbantailor.in>
2016-10-06 00:15:38 +0530

    First Commit while discussion with prashant

---
+++ bower_components/app.js
@@ -0,0 +1,14 @@
+(funtction(){
+
+
+})();
+
+var app = angular.module("app",[]);
+app.controller("MainController",MainController);
+
+function MainController($scope){
+	$scope.message ="Aayushi Jain";
+
+}
+
+}());
